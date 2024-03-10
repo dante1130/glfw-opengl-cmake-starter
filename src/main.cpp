2147788175b49b177bc701b834d4c3ca3ade6f6b
dante1130 <andrewdante2@gmail.com>
2024-03-11 00:47:03 +0800

    Link glfw and create sample window

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,25 @@
-auto main() -> int { return 0; }
+#include <GLFW/glfw3.h>
+
+auto main() -> int {
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    auto* window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
