203304146480f19524555864c0609f806df4d95e
dante1130 <andrewdante2@gmail.com>
2024-03-11 01:19:11 +0800

    Generate and link glad and use OpenGL functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,44 @@
+#include <iostream>
+
+#include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
 auto main() -> int {
     if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW\n";
         return -1;
     }
 
-    auto* window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    auto* window = glfwCreateWindow(640, 480, "Application", nullptr, nullptr);
 
     if (!window) {
+        std::cerr << "Failed to create GLFW window\n";
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
 
+    const auto version = gladLoadGL(glfwGetProcAddress);
+    if (version == 0) {
+        std::cerr << "Failed to initialize GLAD\n";
+        return -1;
+    }
+
+    std::cout << "OpenGL Version " << GLAD_VERSION_MAJOR(version) << "."
+              << GLAD_VERSION_MINOR(version) << " loaded\n";
+
     while (!glfwWindowShouldClose(window)) {
-        glfwSwapBuffers(window);
         glfwPollEvents();
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
     }
 
     glfwTerminate();
